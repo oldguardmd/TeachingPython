2dce46d6b31a192f05dcfd31809fcdd91bfc43a5
oldguarmd <oldguard@msn.com>
2023-10-01 10:40:56 -0400

    Simple Output

---
+++ 001-HelloWorld.py
@@ -0,0 +1,16 @@
+#We all hate hello world. It makes us feel dumb, but if you cannot do this, everything else is a waste of time.
+
+# You Need to know how to do out put to the screen
+print("Hello World")
+
+# You need to know you can print a variable
+someText = "Hello World"
+print("{}".format(someText))
+
+# Never versions of Python let you format that data quicker and with less complexity
+print(f"{someText}")
+
+# And we can do math and print the results
+Something = 1
+SomethingElse = 2
+print(f"The variable Something plus the variable SomethingElse Equals: {Something + SomethingElse}")
